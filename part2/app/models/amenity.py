d77b96384ed89751fd74961f4b83683b82e73e0a
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-17 22:01:28 +0200

    Update model amenity.py

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -4,4 +4,15 @@ from app.models.basemodel import BaseModel as BaseModel
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
+        self.__name = None
         self.name = name
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+        if not value:
+            raise ValueError
+        self.__name = value

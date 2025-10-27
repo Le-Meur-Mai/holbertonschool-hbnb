4277b44bea12b48360253131ae004fc33a5be46b
Le-Meur-Mai <mai.lm@outlook.fr>
2025-10-27 14:31:27 +0100

    Modifying the return message when a user is create

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -50,9 +50,7 @@ class UserList(Resource):
             return {'error': 'Invalid input data'}, 400
 
         return {'id': new_user.id,
-                'first_name': new_user.first_name,
-                'last_name': new_user.last_name,
-                'email': new_user.email}, 201
+                'Success': 'User created successfully !'}, 201
 
     @api.response(200, 'List of users retrieved successfully')
     def get(self):

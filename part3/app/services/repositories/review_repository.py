26d773143704193e5730d8de613c7f2f36a6096a
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-02 16:59:35 +0100

    Correcting the syntax

--- part3/app/services/repositories/review_repository.py
+++ part3/app/services/repositories/review_repository.py
@@ -2,6 +2,6 @@ from app.models.review import Review
 from app import db
 from app.persistence.repository import SQLAlchemyRepository
 
-class UserRepository(SQLAlchemyRepository):
+class ReviewRepository(SQLAlchemyRepository):
     def __init__(self):
         super().__init__(Review)

d5b5be9178ea011fe91e91285eee7e7696b2e16d
Le-Meur-Mai <mai.lm@outlook.fr>
2025-11-02 16:56:46 +0100

    Adding the relation many to many with places and modifying the get amenity by names

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import validates
 from sqlalchemy.orm import relationship
 from app import db
 
+place_amenity = db.Table('place_amenity',
+    Column('place_id', Integer, ForeignKey('places.id'), primary_key=True),
+    Column('amenity_id', Integer, ForeignKey('amenities.id'), primary_key=True)
+)
+
 class Amenity(BaseModel):
     """Model representing an amenity (feature or service) available in a place.
 
@@ -15,6 +20,8 @@ class Amenity(BaseModel):
     """
     __tablename__ = "amenities"
     name = db.Column(db.String(50), nullable=False)
+    places = relationship('Place', secondary=place_amenity, lazy='subquery',
+                          backref=db.backref('amenities', lazy=True))
 
     def __init__(self, name):
         """Initialize a new Amenity instance.
@@ -38,6 +45,6 @@ class Amenity(BaseModel):
         Raises:
             ValueError: If the name is empty or None.
         """
-        if not value or value > 50:
+        if not value or len(value) > 50:
             raise ValueError
         return value
